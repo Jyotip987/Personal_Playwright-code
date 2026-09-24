a6a6a24a3cfa4cd42a8fa154581820c117261d2c
Jyotip987 <jyotiprakashghadei71@gmail.com>
2026-09-24 09:00:57 +0530

    commit 2

--- tests/playwrightME.spec.ts
+++ tests/playwrightME.spec.ts
@@ -3,5 +3,5 @@ import {test } from '@playwright/test'
 test('get multiple element',async({page})=>{
     await page.goto("https://app.thetestingacademy.com/playwright/multiple_element_filter")
     let elements = page.locator('.list-group-item')
-    await elements.first().allTextContents()
+    console.log(await elements.first().allTextContents())
 })
